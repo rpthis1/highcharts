a7e16155456b20ed0db8efb48dbc8c677351672f
me <mprado85@gmail.com>
2014-08-05 15:39:23 -0700

    adding HighCharts with angular start project

---
+++ column-basic/app.js
@@ -0,0 +1,124 @@
+'use strict';
+
+var myapp = angular.module('myapp', ["highcharts-ng"]);
+
+myapp.controller('myctrl', function ($scope) {
+
+  $scope.chartTypes = [
+    {"id": "line", "title": "Line"},
+    {"id": "spline", "title": "Smooth line"},
+    {"id": "area", "title": "Area"},
+    {"id": "areaspline", "title": "Smooth area"},
+    {"id": "column", "title": "Column"},
+    {"id": "bar", "title": "Bar"},
+    {"id": "pie", "title": "Pie"},
+    {"id": "scatter", "title": "Scatter"}
+  ];
+
+  $scope.dashStyles = [
+    {"id": "Solid", "title": "Solid"},
+    {"id": "ShortDash", "title": "ShortDash"},
+    {"id": "ShortDot", "title": "ShortDot"},
+    {"id": "ShortDashDot", "title": "ShortDashDot"},
+    {"id": "ShortDashDotDot", "title": "ShortDashDotDot"},
+    {"id": "Dot", "title": "Dot"},
+    {"id": "Dash", "title": "Dash"},
+    {"id": "LongDash", "title": "LongDash"},
+    {"id": "DashDot", "title": "DashDot"},
+    {"id": "LongDashDot", "title": "LongDashDot"},
+    {"id": "LongDashDotDot", "title": "LongDashDotDot"}
+  ];
+
+  $scope.chartSeries = [
+    {"name": "TMY3 Temperature", "data": [1, 2, 4, 7, 3,9,20], type: "line"},
+    {"name": "Baseline",  data: [{
+        name: 'Point 1',
+        color: '#FC9005',
+        y: 1
+    }, {
+        name: 'Point 2',
+        color: '#FC9005',
+        y: 5
+    }]  , type: "column"},
+    {"name": "Normalized", "data": [1, 1, 2, 3, 2], type: "column"}
+  ];
+
+  $scope.chartStack = [
+    {"id": '', "title": "No"},
+    {"id": "normal", "title": "Normal"},
+    {"id": "percent", "title": "Percent"}
+  ];
+
+  $scope.addPoints = function () {
+    var seriesArray = $scope.chartConfig.series;
+    var rndIdx = Math.floor(Math.random() * seriesArray.length);
+    seriesArray[rndIdx].data = seriesArray[rndIdx].data.concat([1, 10, 20])
+  };
+
+  $scope.addSeries = function () {
+    var rnd = []
+    for (var i = 0; i < 10; i++) {
+      rnd.push(Math.floor(Math.random() * 20) + 1)
+    }
+    $scope.chartConfig.series.push({
+      data: rnd
+    })
+  }
+
+  $scope.removeRandomSeries = function () {
+    var seriesArray = $scope.chartConfig.series;
+    var rndIdx = Math.floor(Math.random() * seriesArray.length);
+    seriesArray.splice(rndIdx, 1)
+  }
+
+  $scope.removeSeries = function (id) {
+    var seriesArray = $scope.chartConfig.series;
+    seriesArray.splice(id, 1)
+  }
+
+  $scope.toggleHighCharts = function () {
+    this.chartConfig.useHighStocks = !this.chartConfig.useHighStocks
+  }
+
+  $scope.replaceAllSeries = function () {
+    var data = [
+      { name: "first", data: [10] },
+      { name: "second", data: [3] },
+      { name: "third", data: [13] }
+    ];
+    $scope.chartConfig.series = data;
+  };
+
+  $scope.chartConfig = {
+    options: {
+      chart: {
+        type: 'areaspline'
+      },
+       xAxis: [{
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+       }],
+
+      plotOptions: {
+        series: {
+          stacking: ''
+        }
+      }
+    },
+    series: $scope.chartSeries,
+    title: {
+      text: 'Hello'
+    },
+    credits: {
+      enabled: true
+    },
+    loading: false,
+    size: {}
+  }
+
+  $scope.reflow = function () {
+    $scope.$broadcast('highchartsng.reflow');
+  };
+
+
+});
